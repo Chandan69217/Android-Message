922c0abc907ee74e969052f241f4ae0b6d507001
Chandan Sharma <chandansharma69217@gmail.com>
2023-12-31 02:38:26 +0530

    new feature added request runtime permission

--- app/src/main/java/com/chandan/message/MainActivity.java
+++ app/src/main/java/com/chandan/message/MainActivity.java
@@ -1,10 +1,19 @@
 package com.chandan.message;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.MenuItemCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.Menu;
 import android.widget.SearchView;
@@ -14,16 +23,90 @@ import com.chandan.message.database.Messages;
 import com.chandan.message.page_viewer.ViewPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
 
+import java.security.Permission;
+
 public class MainActivity extends AppCompatActivity {
     private SearchView searchView;
+    private static  final String[] PERMISSIONS = new String[]{"android.permission.RECEIVE_SMS","android.permission.READ_SMS","android.permission.SEND_SMS"};
+    private static final int PERMISSION_REQ_CODE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestRuntimePermission();
         setContentView(R.layout.activity_main);
         setSupportActionBar((androidx.appcompat.widget.Toolbar)findViewById(R.id.home_toolbar));
         getSupportActionBar().setTitle(0);
         ((ViewPager)findViewById(R.id.view_pager)).setAdapter(new ViewPagerAdapter(getSupportFragmentManager()));
         ((TabLayout)findViewById(R.id.home_tab_layout)).setupWithViewPager((ViewPager)findViewById(R.id.view_pager));
+    }
+
+    private void requestRuntimePermission(){
+        if(ActivityCompat.checkSelfPermission(this,PERMISSIONS[0]) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,PERMISSIONS[1]) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,PERMISSIONS[2]) == PackageManager.PERMISSION_GRANTED){
+
+
+
+        }else if(ActivityCompat.shouldShowRequestPermissionRationale(this,PERMISSIONS[0]) &&
+                ActivityCompat.shouldShowRequestPermissionRationale(this,PERMISSIONS[1]) &&
+                ActivityCompat.shouldShowRequestPermissionRationale(this,PERMISSIONS[2])){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("This app required SMS Permission for particular feature to work as expected")
+                    .setTitle("Permission Required")
+                    .setCancelable(false)
+                    .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            ActivityCompat.requestPermissions(MainActivity.this,PERMISSIONS,PERMISSION_REQ_CODE);
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    })
+                    .show();
+        }else{
+            ActivityCompat.requestPermissions(this,PERMISSIONS,PERMISSION_REQ_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+       if(requestCode == PERMISSION_REQ_CODE){
+           if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+           }else if(!ActivityCompat.shouldShowRequestPermissionRationale(this,PERMISSIONS[0]) &&
+                   !ActivityCompat.shouldShowRequestPermissionRationale(this,PERMISSIONS[1]) &&
+                   !ActivityCompat.shouldShowRequestPermissionRationale(this,PERMISSIONS[2])){
+               AlertDialog.Builder builder = new AlertDialog.Builder(this);
+               builder.setMessage("This app required some permission that you have denied" +
+                       " please allow SMS permission from settings to proceed further")
+                       .setTitle("Permission Required")
+                       .setCancelable(false)
+                       .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialogInterface, int i) {
+                               dialogInterface.dismiss();
+                           }
+                       })
+                       .setPositiveButton("Settings", new DialogInterface.OnClickListener() {
+                           @Override
+                           public void onClick(DialogInterface dialogInterface, int i) {
+                               Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                               Uri uri = Uri.fromParts("package",getPackageName(),null);
+                               intent.setData(uri);
+                               startActivity(intent);
+                           }
+                       })
+                       .show();
+           }else{
+               requestRuntimePermission();
+           }
+       }
     }
 
     @Override
--- app/src/main/java/com/chandan/message/chat_activity/ChatActivity.java
+++ app/src/main/java/com/chandan/message/chat_activity/ChatActivity.java
@@ -1,25 +1,49 @@
 package com.chandan.message.chat_activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.chandan.message.R;
 import com.chandan.message.database.Messages;
 
 public class ChatActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        toolbar = findViewById(R.id.chat_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(0);
 
-        Intent getData = getIntent();
-        Messages messagesData = getData.getParcelableExtra("data");
+        Messages messages = getIntent().getParcelableExtra("data");
+        ((TextView)toolbar.findViewById(R.id.chat_title)).setText(messages.getTitle());
+        ((TextView)toolbar.findViewById(R.id.chat_subtitle)).setText(messages.getSubTitle());
 
-        Log.d("values","MessageID: " + messagesData.getMessageID() + " Message Image: " + messagesData.getImage() + " Message Sender: " + messagesData.getTitle() + " Message Body: " + messagesData.getSubTitle());
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.chat_menu_option,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==android.R.id.home){
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
